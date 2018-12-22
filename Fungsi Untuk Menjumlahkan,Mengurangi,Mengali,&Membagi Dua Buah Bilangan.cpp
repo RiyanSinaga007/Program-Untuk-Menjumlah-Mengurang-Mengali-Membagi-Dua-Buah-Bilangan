997dd8266cbd3d27c3a4c6d5c938856cecebf0e8
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-22 23:51:26 +0700

    Add files via upload

---
+++ Fungsi Untuk Menjumlahkan,Mengurangi,Mengali,&Membagi Dua Buah Bilangan.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int Penjumlahan(){
+	int a,b;
+	cout<<"\n Penjumlahan";
+	cout<<"\n Masukan Nilai Bilangan a = ";
+	cin>>a;
+	cout<<"\n Masukan Nilai Bilangan b = ";
+	cin>>b;
+	cout<<"Hasil Penjumlahan = "<<a+b;
+}
+int Pengurangan(){
+	int a,b;
+	cout<<"\n Pengurangan";
+	cout<<"\n Masukan Nilai Bilangan a = ";
+	cin>>a;
+	cout<<"\n Masukan Nilai Bilangan b = ";
+	cin>>b;
+	cout<<"Hasil Pengurangan = "<<a-b;
+}
+int Perkalian(){
+	int a,b;
+	cout<<"\n Perkalian";
+	cout<<"\n Masukan Nilai Bilangan a = ";
+	cin>>a;
+	cout<<"\n Masukan Nilai Bilangan b = ";
+	cin>>b;
+	cout<<"Hasil Perkalian = "<<a*b;
+}
+int Pembagian(){
+	int a,b;
+	cout<<"\n Pembagian";
+	cout<<"\n Masukan Nilai Bilangan a = ";
+	cin>>a;
+	cout<<"\n Masukan NIlai Bilangan b = ";
+	cin>>b;
+	cout<<"Hasil Pembagian = "<<a/b;
+}
+int main(){
+	cout<<endl;
+	Penjumlahan();
+	Pengurangan();
+	Perkalian();
+	Pembagian();
+	cout<<endl;
+	system("pause");
+}
+	
+	
+	
+	
+	
+	
+	
+	
